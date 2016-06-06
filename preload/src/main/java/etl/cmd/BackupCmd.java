4df0c8f2bebf950b8ce447ab86f3db0b8837153f
phillipcheng <phillipchengyi@gmail.com>
2016-06-06 07:04:06 -0700

    add backupcmd

---
+++ preload/src/main/java/etl/cmd/BackupCmd.java
@@ -0,0 +1,40 @@
+package etl.cmd;
+
+import java.util.List;
+import java.util.Map;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FileUtil;
+import org.apache.hadoop.fs.Path;
+import org.apache.log4j.Logger;
+
+import etl.cmd.dynschema.DynSchemaCmd;
+import etl.engine.ETLCmd;
+import etl.util.Util;
+
+public class BackupCmd extends ETLCmd{
+	public static final Logger logger = Logger.getLogger(BackupCmd.class);
+	
+	private String xmlFolder;
+	private String dataHistoryFolder;
+	
+	public BackupCmd(String wfid, String staticCfg, String inDynCfg, String outDynCfg, String defaultFs){
+		super(wfid, staticCfg, inDynCfg, outDynCfg, defaultFs);
+		this.xmlFolder = pc.getString(DynSchemaCmd.cfgkey_xml_folder);
+		this.dataHistoryFolder = pc.getString(DynSchemaCmd.cfgkey_data_history_folder);
+	}
+	
+	@Override
+	public void process(String param) {
+		List<String> xmlFiles = dynCfgMap.get(DynSchemaCmd.dynCfg_Key_XML_FILES);
+		try {
+			for (String xmlFile: xmlFiles){
+				FileUtil.copy(fs, new Path(xmlFolder+xmlFile), fs, new Path(dataHistoryFolder+xmlFile), true, this.getHadoopConf());
+				logger.info(String.format("copy and remove %s to %s", xmlFolder+xmlFile, dataHistoryFolder+xmlFile));
+			}
+		}catch(Exception e){
+			logger.error("", e);
+		}
+	}
+}
