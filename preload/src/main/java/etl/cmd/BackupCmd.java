45ebdc72d0bfad491ca36cb30154fc772a926901
rajashankary <rajashankar.yaligar@hpe.com>
2016-06-23 21:50:03 +0530

    Changes done for folder filter and file filter.

--- preload/src/main/java/etl/cmd/BackupCmd.java
+++ preload/src/main/java/etl/cmd/BackupCmd.java
@@ -1,28 +1,20 @@
 package etl.cmd;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
-
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.FileUtil;
-import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.fs.RemoteIterator;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -39,58 +31,75 @@ public class BackupCmd extends ETLCmd{
 	public static final String cfgkey_csv_folder="csv-folder";
 	public static final String cfgkey_data_history_folder="data-history-folder";
 	public static final String cfgkey_destination_zip_folder="destination-zip-folder";
+	public static final String cfgkey_Folder_filter="folder.filter";
+	public static final String cfgkey_file_filter="file.filter";
+	public static final String defaultFolderFilter="defaultFolder.filter";
 
 	private String xmlFolder;
 	private String csvFolder;
 	private String dataHistoryFolder;
 	private String destinationZipFolder;
 	private FileSystem fileSys;
+	private String inDynCfg;
+	private String[] folderFilter;
+	private String[] fileFilter;
+	private String userFilter;
+	private ZipOutputStream zos;
 
 	public BackupCmd(String wfid, String staticCfg, String inDynCfg, String outDynCfg, String defaultFs){
 		super(wfid, staticCfg, inDynCfg, outDynCfg, defaultFs);
-		this.xmlFolder = pc.getString(BackupCmd.cfgkey_xml_folder);
-		this.csvFolder = pc.getString(BackupCmd.cfgkey_csv_folder);
-		this.dataHistoryFolder = pc.getString(BackupCmd.cfgkey_data_history_folder);
+
+		this.dataHistoryFolder = pc.getString(cfgkey_data_history_folder);
+		this.folderFilter = pc.getStringArray(cfgkey_Folder_filter);
+		this.fileFilter = pc.getStringArray(cfgkey_file_filter);
 		this.destinationZipFolder=this.dataHistoryFolder+wfid+".zip";
+		this.xmlFolder = folderFilter[0];
+		this.csvFolder = folderFilter[1];
+		this.userFilter=fileFilter[0];
+		this.inDynCfg=inDynCfg;
 	}
 
 	@Override
 	public List<String> process(String param, Mapper<Object, Text, Text, NullWritable>.Context context) {
-		ZipOutputStream zos = null;
-		logger.info("param: %"+param);
+
+		logger.info("param: "+param);
+
 		Map<String, String> pm = Util.parseMapParams(param);
-		if (pm.containsKey(BackupCmd.cfgkey_xml_folder)){
-			this.xmlFolder = pm.get(BackupCmd.cfgkey_xml_folder);
+		if (pm.containsKey(cfgkey_xml_folder)){
+			this.xmlFolder = pm.get(cfgkey_xml_folder);
 		}
-		if (pm.containsKey(BackupCmd.cfgkey_csv_folder)){
-			this.csvFolder = pm.get(BackupCmd.cfgkey_csv_folder);
+		if (pm.containsKey(cfgkey_csv_folder)){
+			this.csvFolder = pm.get(cfgkey_csv_folder);
 		}
-		if (pm.containsKey(BackupCmd.cfgkey_data_history_folder)){
-			this.dataHistoryFolder = pm.get(BackupCmd.cfgkey_data_history_folder);
+		if (pm.containsKey(cfgkey_data_history_folder)){
+			this.dataHistoryFolder = pm.get(cfgkey_data_history_folder);
 		}
-		if (pm.containsKey(BackupCmd.cfgkey_destination_zip_folder)){
-			this.destinationZipFolder = pm.get(BackupCmd.cfgkey_destination_zip_folder);
+		if (pm.containsKey(cfgkey_destination_zip_folder)){
+			this.destinationZipFolder = pm.get(cfgkey_destination_zip_folder);
+		}
+		if (pm.containsKey(cfgkey_Folder_filter)){
+
+			this.folderFilter=pm.get(cfgkey_Folder_filter).split(" ");
+		}
+		if (pm.containsKey(cfgkey_file_filter)){
+			this.userFilter = pm.get(cfgkey_file_filter);
 		}
 
-		// Code to Zip the xml and csv files.
 		try {
-
-			logger.info("Starting Job to Zip files for wfid :  "+wfid);
-			String[] dir={xmlFolder,csvFolder};
 			fileSys=FileSystem.get(this.getHadoopConf());
 			if((!(destinationZipFolder.startsWith(dataHistoryFolder)&&destinationZipFolder.endsWith(".zip"))))
 			{
-				logger.info("The Destination Zip folder provided is not proper for wfid "+wfid);
+				logger.info("The Destination Zip folder provided is not valid for wfid "+wfid);
 				return null;
 			}
 			Path destpath=new Path(destinationZipFolder);
 			FSDataOutputStream fos = fileSys.create(destpath);
 			zos = new ZipOutputStream(fos);	
-			for (int i = 0; i < dir.length; i++) 
+			for (int i = 0; i < folderFilter.length; i++) 
 			{
-				zipFolder(dir[i],zos);	
+				zipFolder(folderFilter[i]);	
 			}
-			logger.info("Finished Job of Zip files for wfid :  "+wfid);
+			logger.info("Finished Job :  "+wfid);
 		} 
 		catch (NullPointerException e) {
 			logger.error("Null Pointer exception ...!  ", e);
@@ -112,54 +121,54 @@ public class BackupCmd extends ETLCmd{
 			}
 		}
 
-		//Copy and remove xml file code ( existing code )
-		List<String> xmlFiles = dynCfgMap.get(DynSchemaCmd.dynCfg_Key_XML_FILES);
-		try {
-			for (String xmlFile: xmlFiles){
-				FileUtil.copy(fs, new Path(xmlFolder+xmlFile), fs, new Path(dataHistoryFolder+xmlFile), true, this.getHadoopConf());
-				logger.info(String.format("copy and remove %s to %s", xmlFolder+xmlFile, dataHistoryFolder+xmlFile));
+		//	Copy and remove xml file code ( existing code )
+		if(inDynCfg!=null)
+		{
+			logger.error("Dynamic Cfg is not null ...! ");
+			List<String> xmlFiles = dynCfgMap.get(DynSchemaCmd.dynCfg_Key_XML_FILES);
+			try {
+				for (String xmlFile: xmlFiles){
+					FileUtil.copy(fs, new Path(xmlFolder+xmlFile), fs, new Path(dataHistoryFolder+xmlFile), true, this.getHadoopConf());
+					logger.info(String.format("copy and remove %s to %s", xmlFolder+xmlFile, dataHistoryFolder+xmlFile));
 
+				}
 			}
+
+			catch(Exception e){
+				logger.error("", e);
+			}	
 		}
 
-		catch(Exception e){
-			logger.error("", e);
-		}
 		return null;
 	}
 
-
-	public  void zipFolder(String dirpath, ZipOutputStream zos)
+	public  void zipFolder(String dirpath)
 	{
-
 		try {
 			Path inputPath = new Path(dirpath);
 			fileSys = inputPath.getFileSystem(this.getHadoopConf());
 			FileStatus[] status = fileSys.listStatus(inputPath);
-			logger.info("Files exist in the folder "+dirpath+": "+(status.length>0));
 			for (int i=0;i<status.length;i++){
 				Path path =status[i].getPath();
-				logger.info("Checking files path:"+path);
 				if(status[i].isFile()){
 					String fileName=path.getName();
-					if(dirpath.equals(xmlFolder) || (fileName.startsWith(wfid, 0)&&(fileName.endsWith(".csv"))))
+					if(dirpath.equals(xmlFolder)&&(userFilter.equals(fileFilter[0])||userFilter.equals(fileFilter[2])))
 					{
-						logger.info("File Name: "+fileName);   
-						ZipEntry ze= new ZipEntry(fileName);
-						zos.putNextEntry(ze);
-						Path srcpath=new Path(dirpath+ File.separator + fileName);
-						logger.info("src path is "+srcpath);
-						FSDataInputStream in = fileSys.open(srcpath);
-						int buffersize =8192;
-						byte[] buffer = new byte[buffersize];
-						int count;
-						while((count = in.read(buffer)) != -1)
-						{
-							zos.write(buffer,0,count);    
-						}
-						zos.closeEntry();
-						in.close();
+						zipFile(dirpath,fileName);
 					}
+					else if(dirpath.equals(csvFolder)&&(userFilter.equals(fileFilter[1])||userFilter.equals(fileFilter[2])))
+					{
+						if(fileName.startsWith(wfid, 0))
+						{
+							zipFile(dirpath,fileName);
+						}
+					}
+
+					else if(userFilter.equals(fileFilter[2]))
+					{
+						zipFile(dirpath,fileName);	
+					}
+
 				}
 			}
 		}
@@ -178,6 +187,44 @@ public class BackupCmd extends ETLCmd{
 			// TODO: handle exception
 			logger.error(" ", e);
 		}
+	}
+
+	public void zipFile(String dirpath,String fileName)
+	{
+
+		try {
+			logger.info("Adding file "+fileName); 
+			ZipEntry ze= new ZipEntry(fileName);
+			zos.putNextEntry(ze);
+			Path srcpath=new Path(dirpath+ File.separator + fileName);
+			logger.info("src path is "+srcpath);
+			FSDataInputStream in = fileSys.open(srcpath);
+			int buffersize =8192;
+			byte[] buffer = new byte[buffersize];
+			int count;
+			while((count = in.read(buffer)) != -1)
+			{
+				zos.write(buffer,0,count);    
+			}
+			zos.closeEntry();
+			in.close();
+
+		} 	catch(FileNotFoundException e)
+		{
+			logger.error("Folder does not exists .! Exception occured...! ", e);
+		}
+		catch (NullPointerException e) {
+			logger.error("Null Pointer exception ...!  ", e);
+		}
+		catch (IOException e) {
+			// TODO: handle exception
+			logger.error("File IO exception occured...! ", e);
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			logger.error(" ", e);
+		}
+
 	}
 
 }
--- preload/src/test/java/etl/cmd/test/BackUPCmdTest.java
+++ preload/src/test/java/etl/cmd/test/BackUPCmdTest.java
@@ -1,73 +1,75 @@
 package etl.cmd.test;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FilenameFilter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URI;
 import java.security.PrivilegedExceptionAction;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
-
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FSDataOutputStream;
-import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.fs.RemoteIterator;
 import org.apache.hadoop.security.UserGroupInformation;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.apache.hadoop.conf.Configuration;
-
 import etl.cmd.BackupCmd;
-import etl.cmd.SftpCmd;
-import etl.engine.InvokeMapper;
-import static org.junit.Assert.*;
 import org.apache.log4j.Logger;
 
 public class BackUPCmdTest {
-	public Configuration conf = new Configuration();
+	
 	public static final Logger logger = Logger.getLogger(BackUPCmdTest.class);
-	String defaultFS =null;
-	String wfid=null;
+	private Configuration conf;
+	private String wfid=null;
+	private String dynconf=null;
+	private String localFolder;
+	private String dfsFolder;
+	private String defaultFS;
+	private String cfg;
+	private FileSystem fs;
 
 	/** * Initialization */
 	@Before
 	public void setUp() {
-
+		conf = new Configuration();
+	    defaultFS = "hdfs://192.85.247.104:19000";
+		conf.set("fs.defaultFS", defaultFS);
+	    localFolder = "C:\\Users\\yaligar\\git\\log.analysis\\mtccore\\src\\main\\resources\\";
+		dfsFolder = "/test_folder/";
+	    cfg = "sgsiwf.backup.properties";
 		wfid="0000065-162525414258816-oozie-dbad-W"; //use wfid as per necessity
+		dynconf="/mtccore/schemahistory/sgsiwf.dyncfg_0000083-160525114258816-oozie-dbad-W";
 	}
 
-	@Test
+	@Test 
+	// Testing of xml-folder [ Not a folder filter ].
 	public void test1_XmlFolder() throws Exception {
 
 		UserGroupInformation ugi = UserGroupInformation.createProxyUser("dbadmin", UserGroupInformation.getLoginUser());
 		ugi.doAs(new PrivilegedExceptionAction<Void>() {
 			public Void run() throws Exception {
 				try {
-
-
-					Configuration conf = new Configuration();
-					String defaultFS = "hdfs://192.85.247.104:19000";
-					conf.set("fs.defaultFS", defaultFS);
-					FileSystem fs = FileSystem.get(conf);
-					String localFolder = "C:\\Users\\yaligar\\git\\log.analysis\\mtccore\\src\\main\\resources\\";
-					String dfsFolder = "/test_folder/";
-					String cfg = "sgsiwf.backup1.properties";
+					fs = FileSystem.get(conf);
 					fs.copyFromLocalFile(new Path(localFolder + cfg), new Path(dfsFolder+cfg));
 					BackupCmd cmd = new BackupCmd(wfid, dfsFolder+cfg, null, null, defaultFS);
-					cmd.process("xml-folder=/test_folder/xmldata", null);
+					cmd.process("xml-folder=/test_folder/xmldata/", null);
 
 				} catch (Exception e) {
-					// TODO: handle exception
+					logger.error("", e);
+				}
+				return null;
+			}
+		});
+	}
+	@Test 
+	// Testing with  csv-folder [ Not a folder filter ].
+	public void test2_CsvFolder() throws Exception {
+
+		UserGroupInformation ugi = UserGroupInformation.createProxyUser("dbadmin", UserGroupInformation.getLoginUser());
+		ugi.doAs(new PrivilegedExceptionAction<Void>() {
+			public Void run() throws Exception {
+				try {
+					fs = FileSystem.get(conf);
+					fs.copyFromLocalFile(new Path(localFolder + cfg), new Path(dfsFolder+cfg));
+					BackupCmd cmd = new BackupCmd(wfid, dfsFolder+cfg, null, null, defaultFS);
+					cmd.process("csv-folder=/test_folder/csvdata/", null);
+
+				} catch (Exception e) {
+					logger.error("", e);
 				}
 				return null;
 			}
@@ -75,50 +77,19 @@ public class BackUPCmdTest {
 	}
 
 	@Test
-	public void test1_CsvFolder() throws Exception {
+	// Testing with data-history-folder.
+	public void test3_DataHistory() throws Exception {
 
 		UserGroupInformation ugi = UserGroupInformation.createProxyUser("dbadmin", UserGroupInformation.getLoginUser());
 		ugi.doAs(new PrivilegedExceptionAction<Void>() {
 			public Void run() throws Exception {
 				try {
-
-					Configuration conf = new Configuration();
-					String defaultFS = "hdfs://192.85.247.104:19000";
-					conf.set("fs.defaultFS", defaultFS);
-					FileSystem fs = FileSystem.get(conf);
-					String localFolder = "C:\\Users\\yaligar\\git\\log.analysis\\mtccore\\src\\main\\resources\\";
-					String dfsFolder = "/test_folder/";
-					String cfg = "sgsiwf.backup1.properties";
-					fs.copyFromLocalFile(new Path(localFolder + cfg), new Path(dfsFolder+cfg));
-					BackupCmd cmd = new BackupCmd(wfid, dfsFolder+cfg, null, null, defaultFS);
-					cmd.process("csv-folder=/test_folder/csvdat", null);
-				} catch (Exception e) {
-					// TODO: handle exception
-				}
-				return null;
-			}
-		});
-	}
-	@Test
-	public void test1_DataHistory() throws Exception {
-
-		UserGroupInformation ugi = UserGroupInformation.createProxyUser("dbadmin", UserGroupInformation.getLoginUser());
-		ugi.doAs(new PrivilegedExceptionAction<Void>() {
-			public Void run() throws Exception {
-				try {
-
-					Configuration conf = new Configuration();
-					String defaultFS = "hdfs://192.85.247.104:19000";
-					conf.set("fs.defaultFS", defaultFS);
-					FileSystem fs = FileSystem.get(conf);
-					String localFolder = "C:\\Users\\yaligar\\git\\log.analysis\\mtccore\\src\\main\\resources\\";
-					String dfsFolder = "/test_folder/";
-					String cfg = "sgsiwf.backup1.properties";
+					fs = FileSystem.get(conf);
 					fs.copyFromLocalFile(new Path(localFolder + cfg), new Path(dfsFolder+cfg));
 					BackupCmd cmd = new BackupCmd(wfid, dfsFolder+cfg, null, null, defaultFS);
 					cmd.process("data-history-folder=/test_folder/dataHistory/", null);
-				} catch (Exception e) {
-					// TODO: handle exception
+				    } catch (Exception e) {
+					logger.error("", e);
 				}
 				return null;
 			}
@@ -126,31 +97,211 @@ public class BackUPCmdTest {
 	}
 
 	@Test
-	public void test1_destinationZipFolder() throws Exception {
+	// Testing with destination-Zip-Folder.
+	public void test4_destinationZipFolder() throws Exception {
 
 		UserGroupInformation ugi = UserGroupInformation.createProxyUser("dbadmin", UserGroupInformation.getLoginUser());
 		ugi.doAs(new PrivilegedExceptionAction<Void>() {
 			public Void run() throws Exception {
 				try {
-					Configuration conf = new Configuration();
-					String defaultFS = "hdfs://192.85.247.104:19000";
-					conf.set("fs.defaultFS", defaultFS);
-					FileSystem fs = FileSystem.get(conf);
-					String localFolder = "C:\\Users\\yaligar\\git\\log.analysis\\mtccore\\src\\main\\resources\\";
-					String dfsFolder = "/test_folder/";
-					String cfg = "sgsiwf.backup1.properties";
+					fs = FileSystem.get(conf);
 					fs.copyFromLocalFile(new Path(localFolder + cfg), new Path(dfsFolder+cfg));
 					BackupCmd cmd = new BackupCmd(wfid, dfsFolder+cfg, null, null, defaultFS);
 					cmd.process("destination-zip-folder=/test_folder/dataHistory/"+wfid+".zip", null);
-				
 				} 
 			catch (Exception e) {
 					// TODO: handle exception
+				logger.error("", e);
 				}
 				return null;
 			}
 		});
 	}
 
+	@Test
+	// Testing with folder.filter xml-folder and default file filter.
+	public void test5_xmlfolder_defaultDynamicFilter() throws Exception {
+		UserGroupInformation ugi = UserGroupInformation.createProxyUser("dbadmin", UserGroupInformation.getLoginUser());
+		ugi.doAs(new PrivilegedExceptionAction<Void>() {
+			public Void run() throws Exception {
+				try {
+					fs = FileSystem.get(conf);
+					fs.copyFromLocalFile(new Path(localFolder + cfg), new Path(dfsFolder+cfg));
+					BackupCmd cmd = new BackupCmd(wfid, dfsFolder+cfg, null, null, defaultFS);
+					cmd.process("folder.filter=/test_folder/xmldata/", null);
+				} 
+			catch (Exception e) {
+					// TODO: handle exception
+				logger.error("", e);
+				}
+				return null;
+			}
+		});
+	}
+	
+	@Test
+	// Testing with folder.filter xml-folder and WFID file filter.
+	public void test6_xmlfolder_WFID_fileFilter() throws Exception {
+		UserGroupInformation ugi = UserGroupInformation.createProxyUser("dbadmin", UserGroupInformation.getLoginUser());
+		ugi.doAs(new PrivilegedExceptionAction<Void>() {
+			public Void run() throws Exception {
+				try {
+					fs = FileSystem.get(conf);
+					fs.copyFromLocalFile(new Path(localFolder + cfg), new Path(dfsFolder+cfg));
+					BackupCmd cmd = new BackupCmd(wfid, dfsFolder+cfg, null, null, defaultFS);
+					cmd.process("folder.filter=/test_folder/xmldata/,file.filter=WFID", null);
+				} 
+			catch (Exception e) {
+					// TODO: handle exception
+				logger.error("", e);
+				}
+				return null;
+			}
+		});
+	}
+	
+	@Test
+	// Testing with folder.filter xml-folder and ALL file filter.
+	public void test7_xmlfolder_ALL_fileFilter() throws Exception {
+		UserGroupInformation ugi = UserGroupInformation.createProxyUser("dbadmin", UserGroupInformation.getLoginUser());
+		ugi.doAs(new PrivilegedExceptionAction<Void>() {
+			public Void run() throws Exception {
+				try {
+					fs = FileSystem.get(conf);
+					fs.copyFromLocalFile(new Path(localFolder + cfg), new Path(dfsFolder+cfg));
+					BackupCmd cmd = new BackupCmd(wfid, dfsFolder+cfg, null, null, defaultFS);
+					cmd.process("folder.filter=/test_folder/xmldata/,file.filter=ALL", null);
+				} 
+			catch (Exception e) {
+					// TODO: handle exception
+				logger.error("", e);
+				}
+				return null;
+			}
+		});
+	}
+	
+	@Test
+	// Testing with folder.filter csv-folder and default file filter.
+	public void test7_csvfolder_defaultfileFilter() throws Exception {
 
+		UserGroupInformation ugi = UserGroupInformation.createProxyUser("dbadmin", UserGroupInformation.getLoginUser());
+		ugi.doAs(new PrivilegedExceptionAction<Void>() {
+			public Void run() throws Exception {
+				try {
+					fs = FileSystem.get(conf);
+					fs.copyFromLocalFile(new Path(localFolder + cfg), new Path(dfsFolder+cfg));
+					BackupCmd cmd = new BackupCmd(wfid, dfsFolder+cfg, null, null, defaultFS);
+					cmd.process("folder.filter=/test_folder/csvdata/", null);
+				} 
+			catch (Exception e) {
+					// TODO: handle exception
+				logger.error("", e);
+				}
+				return null;
+			}
+		});
+	}
+	
+	
+	@Test
+	// Testing with folder.filter csv-folder and WFID file filter.
+	public void test8_csvfolder_WFID_fileFilter() throws Exception {
+
+		UserGroupInformation ugi = UserGroupInformation.createProxyUser("dbadmin", UserGroupInformation.getLoginUser());
+		ugi.doAs(new PrivilegedExceptionAction<Void>() {
+			public Void run() throws Exception {
+				try {
+					fs = FileSystem.get(conf);
+					fs.copyFromLocalFile(new Path(localFolder + cfg), new Path(dfsFolder+cfg));
+					BackupCmd cmd = new BackupCmd(wfid, dfsFolder+cfg, null, null, defaultFS);
+					cmd.process("folder.filter=/test_folder/csvdata/,file.filter=WFID", null);
+				} catch (Exception e) {
+					logger.error("", e);
+				}
+				return null;
+			}
+		});
+	}
+	@Test
+	// Testing with folder.filter csv-folder and ALL file filter.
+	public void test9_csvfolder_ALL_fileFilter() throws Exception {
+
+		UserGroupInformation ugi = UserGroupInformation.createProxyUser("dbadmin", UserGroupInformation.getLoginUser());
+		ugi.doAs(new PrivilegedExceptionAction<Void>() {
+			public Void run() throws Exception {
+				try {
+					fs = FileSystem.get(conf);
+					fs.copyFromLocalFile(new Path(localFolder + cfg), new Path(dfsFolder+cfg));
+					BackupCmd cmd = new BackupCmd(wfid, dfsFolder+cfg, null, null, defaultFS);
+					cmd.process("folder.filter=/test_folder/csvdata/,file.filter=ALL", null);
+				} catch (Exception e) {
+					logger.error("", e);
+				}
+				return null;
+			}
+		});
+	}
+	@Test
+	// Testing with folder.filter default and ALL file filter.
+	public void test10_default_folderfilter_ALL_fileFilter() throws Exception {
+
+		UserGroupInformation ugi = UserGroupInformation.createProxyUser("dbadmin", UserGroupInformation.getLoginUser());
+		ugi.doAs(new PrivilegedExceptionAction<Void>() {
+			public Void run() throws Exception {
+				try {
+					fs = FileSystem.get(conf);
+					fs.copyFromLocalFile(new Path(localFolder + cfg), new Path(dfsFolder+cfg));
+					BackupCmd cmd = new BackupCmd(wfid, dfsFolder+cfg, null, null, defaultFS);
+					cmd.process("file.filter=ALL", null);
+				} catch (Exception e) {
+					logger.error("", e);
+				}
+				return null;
+			}
+		});
+	}
+	@Test
+	// Testing with New folder as folder.filter  and WFID file filter.
+	public void test11_New_folder_WFID_fileFilter() throws Exception {
+
+		UserGroupInformation ugi = UserGroupInformation.createProxyUser("dbadmin", UserGroupInformation.getLoginUser());
+		ugi.doAs(new PrivilegedExceptionAction<Void>() {
+			public Void run() throws Exception {
+				try {
+					fs = FileSystem.get(conf);
+					fs.copyFromLocalFile(new Path(localFolder + cfg), new Path(dfsFolder+cfg));
+					BackupCmd cmd = new BackupCmd(wfid, dfsFolder+cfg, null, null, defaultFS);
+					cmd.process("folder.filter=/test_folder/datafolder3,file.filter=WFID", null);
+				} catch (Exception e) {
+					logger.error("", e);
+				}
+				return null;
+			}
+		});
+	}
+	
+	@Test
+	// Testing with New folder.filter and ALL file filter.
+	public void test11_New_folder_default_fileFilter() throws Exception {
+
+		UserGroupInformation ugi = UserGroupInformation.createProxyUser("dbadmin", UserGroupInformation.getLoginUser());
+		ugi.doAs(new PrivilegedExceptionAction<Void>() {
+			public Void run() throws Exception {
+				try {
+					fs = FileSystem.get(conf);
+					fs.copyFromLocalFile(new Path(localFolder + cfg), new Path(dfsFolder+cfg));
+					BackupCmd cmd = new BackupCmd(wfid, dfsFolder+cfg, null, null, defaultFS);
+					cmd.process("folder.filter=/test_folder/datafolder3,file.filter=ALL", null);
+				} catch (Exception e) {
+					logger.error("", e);
+				}
+				return null;
+			}
+		});
+	}
+
+	
+	
+	
 }
