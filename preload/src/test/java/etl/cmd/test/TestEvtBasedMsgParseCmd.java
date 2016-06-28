aaed7a11654b12654a500bfe10827205a46be3d2
phillipcheng <phillipchengyi@gmail.com>
2016-06-28 21:56:48 +0800

    use super class

--- preload/src/test/java/etl/cmd/test/TestEvtBasedMsgParseCmd.java
+++ preload/src/test/java/etl/cmd/test/TestEvtBasedMsgParseCmd.java
@@ -14,40 +14,34 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
+import etl.engine.ETLCmd;
 import etl.engine.InvokeMapper;
 
-public class TestEvtBasedMsgParseCmd {
+public class TestEvtBasedMsgParseCmd extends TestETLCmd{
 	public static final Logger logger = Logger.getLogger(TestEvtBasedMsgParseCmd.class);
 	public static SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
 	
 	@Test
 	public void test1(){
 		try{
-			Configuration conf = new Configuration();
-			String defaultFS = "hdfs://127.0.0.1:19000";
-			conf.set("fs.defaultFS", defaultFS);
 			String remoteCfgFolder = "/etltest/cfg/";
 			String remoteCsvFolder = "/etltest/msgparse/";
 			String remoteCsvOutputFolder = "/etltest/msgparseout/";
-			FileSystem fs = FileSystem.get(conf);
-			//setup testing env
-			//String localResourceFolder = "/Users/chengyi/git/log.analysis/preload/src/test/resources/";
-			String localResourceFolder = "C:\\mydoc\\myprojects\\log.analysis\\preload\\src\\test\\resources\\";
-			String csvtransProp = "msgparse.properties";
+			String csvtransProp = "hlr.msgparse.singlecmd.properties";
 			String[] csvFiles = new String[]{"input.hlr1.txt"};
-			fs.mkdirs(new Path(remoteCfgFolder));
-			fs.mkdirs(new Path(remoteCsvFolder));
-			fs.delete(new Path(remoteCsvFolder), true);
-			fs.copyFromLocalFile(new Path(localResourceFolder+csvtransProp), new Path(remoteCfgFolder+csvtransProp));
+			getFs().mkdirs(new Path(remoteCfgFolder));
+			getFs().mkdirs(new Path(remoteCsvFolder));
+			getFs().delete(new Path(remoteCsvFolder), true);
+			getFs().copyFromLocalFile(new Path(getLocalFolder()+csvtransProp), new Path(remoteCfgFolder+csvtransProp));
 			for (String csvFile:csvFiles){
-				fs.copyFromLocalFile(new Path(localResourceFolder+csvFile), new Path(remoteCsvFolder+csvFile));
+				getFs().copyFromLocalFile(new Path(getLocalFolder()+csvFile), new Path(remoteCsvFolder+csvFile));
 			}
-			fs.delete(new Path(remoteCsvOutputFolder), true);
+			getFs().delete(new Path(remoteCsvOutputFolder), true);
 			//run job
-			conf.set(InvokeMapper.cfgkey_cmdclassname, "etl.cmd.transform.EvtBasedMsgParseCmd");
-			conf.set(InvokeMapper.cfgkey_wfid, sdf.format(new Date()));
-			conf.set(InvokeMapper.cfgkey_staticconfigfile, remoteCfgFolder+csvtransProp);
-			Job job = Job.getInstance(conf, "testCsvTransformCmd");
+			getConf().set(InvokeMapper.cfgkey_cmdclassname, "etl.cmd.transform.EvtBasedMsgParseCmd");
+			getConf().set(InvokeMapper.cfgkey_wfid, sdf.format(new Date()));
+			getConf().set(InvokeMapper.cfgkey_staticconfigfile, remoteCfgFolder+csvtransProp);
+			Job job = Job.getInstance(getConf(), "testCsvTransformCmd");
 			job.setMapperClass(etl.engine.InvokeMapper.class);
 			job.setNumReduceTasks(0);//no reducer
 			job.setOutputKeyClass(Text.class);
