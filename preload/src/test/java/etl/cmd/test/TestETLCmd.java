9c0e62b19c1c038dc52131a04354a8fde6fa2beb
phillipcheng <phillipchengyi@gmail.com>
2016-06-30 06:33:43 +0800

    change setup

--- preload/src/test/java/etl/cmd/test/TestETLCmd.java
+++ preload/src/test/java/etl/cmd/test/TestETLCmd.java
@@ -1,15 +1,19 @@
 package etl.cmd.test;
 
 import java.io.InputStream;
+import java.security.PrivilegedExceptionAction;
 import java.util.Properties;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.log4j.Logger;
 import org.junit.Before;
 
 public class TestETLCmd {
 	public static final Logger logger = Logger.getLogger(TestETLCmd.class);
+	
+	public static final String remoteUser = "dbadmin";
 	private static String cfgProperties="testETLCmd.properties";
 	private static String key_localFolder="localFolder";
 	private static String key_defaultFs="defaultFS";
@@ -31,7 +35,17 @@ public class TestETLCmd {
 				conf = new Configuration();
 				defaultFS = p.getProperty(key_defaultFs);
 				conf.set("fs.defaultFS", defaultFS);
-				fs = FileSystem.get(conf);
+				if (defaultFS.contains("127.0.0.1")){
+					fs = FileSystem.get(conf);
+				}else{
+					UserGroupInformation ugi = UserGroupInformation.createProxyUser("dbadmin", UserGroupInformation.getLoginUser());
+					ugi.doAs(new PrivilegedExceptionAction<Void>() {
+						public Void run() throws Exception {
+							fs = FileSystem.get(conf);
+							return null;
+						}
+					});
+				}
 			}else{
 				logger.error(String.format("%s not found in classpath.", cfgProperties));
 			}
