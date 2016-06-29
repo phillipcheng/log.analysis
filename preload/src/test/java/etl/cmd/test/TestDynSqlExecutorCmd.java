02844aeee92cca849f0d8f579e28029e92e8f601
punithavallirs <punithavalli11@gmail.com>
2016-06-29 20:37:09 +0530

    sample

--- preload/src/test/java/etl/cmd/test/TestDynSqlExecutorCmd.java
+++ preload/src/test/java/etl/cmd/test/TestDynSqlExecutorCmd.java
@@ -2,15 +2,21 @@ package etl.cmd.test;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.security.PrivilegedExceptionAction;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.security.UserGroupInformation;
 import org.junit.Test;
 
 import etl.cmd.dynschema.DynSchemaCmd;
 import etl.cmd.dynschema.DynSqlExecutorCmd;
+import etl.util.DBUtil;
 import etl.util.Util;
 
 import org.apache.log4j.Logger;
@@ -20,13 +26,16 @@ public class TestDynSqlExecutorCmd extends TestETLCmd{
 	public static final Logger logger = Logger.getLogger(TestDynSqlExecutorCmd.class);
 
 	@Test
-	public void test1(){
+	public void test1() throws Exception{
+		UserGroupInformation ugi = UserGroupInformation.createProxyUser("dbadmin", UserGroupInformation.getLoginUser());
+		ugi.doAs(new PrivilegedExceptionAction<Void>() {
+			public Void run() throws Exception {
 		try {
 			//
 			String inputFolder = "/test/dynsqlexecutor/input/";
 			String dfsCfgFolder = "/test/dynsqlexecutor/cfg/";
 			String schemaFolder="/test/dynsqlexecutor/schema/";
-			
+					
 			String staticCfgName = "dynsqlexecutor1.properties";
 			String wfid="wfid1";
 			String prefix = "sgsiwf";
@@ -57,13 +66,58 @@ public class TestDynSqlExecutorCmd extends TestETLCmd{
 			//run cmd
 			DynSqlExecutorCmd cmd = new DynSqlExecutorCmd(wfid, dfsCfgFolder + staticCfgName, dfsCfgFolder+dynInCfgName, null, getDefaultFS());
 			cmd.process(0, null, null);
-			
+		
 			//check results
-			//check db
 			
+			//schemaFolder should have the schema file
+			List<String> files = Util.listDfsFile(getFs(), schemaFolder);
+			String schemaFileName = String.format("%s.schemas.txt", prefix);
+			assertTrue(files.size()==1);
+			assertTrue(files.contains(schemaFileName));
+					
+			List<String> files1 = Util.listDfsFile(getFs(), inputFolder);
+			String csvFileName = String.format("%s_%s.csv", wfid, "MyCore_");
+			assertTrue(files1.size()==1);
+			assertTrue(files1.contains(csvFileName));
+		
+		
+			//checking create table already created
+			String sql = "SELECT * from sgsiwf.MyCore_;";
+			PropertiesConfiguration pc = Util.getPropertiesConfigFromDfs(getFs(), dfsCfgFolder + staticCfgName);
+			int result=DBUtil.checkSqls(sql,pc);
+			logger.info("The result is file "+result);
+			assertTrue(result==1);
+			
+			//csvData
+			String line;
+			BufferedReader br = new BufferedReader(new FileReader(getLocalFolder() + localCsvFileName));
+			ArrayList<String> csvData=new ArrayList<String>();
+			while ((line = br.readLine()) != null) {
+			    String colValue = line;
+			    logger.info(colValue);
+			    csvData.add(colValue);  
+			}
+			logger.info("The CSV File data is: ");
+			for (String csvdata : csvData)
+			{
+				logger.info(csvdata);
+			}
+			//table data
+			ArrayList<String> cols=new ArrayList<String>() ;
+			cols=DBUtil.checkCsv(sql, pc);
+			logger.info("The Table data is: ");
+			for (String tbdata : cols)
+			{
+				logger.info(tbdata);
+			}
+			assertTrue(cols.containsAll(csvData));
 		} catch (Exception e) {
 			logger.error("Exception occured due to invalid data-history path", e);
 		}
+		return null;
+			}
+		});
+		
 	}
 	
 	@Test
--- preload/src/test/java/etl/cmd/test/TestETLCmd.java
+++ preload/src/test/java/etl/cmd/test/TestETLCmd.java
@@ -1,44 +1,53 @@
 package etl.cmd.test;
 
 import java.io.InputStream;
+import java.security.PrivilegedExceptionAction;
 import java.util.Properties;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.log4j.Logger;
 import org.junit.Before;
+import org.junit.Test;
 
 public class TestETLCmd {
 	public static final Logger logger = Logger.getLogger(TestETLCmd.class);
-	private static String cfgProperties="testETLCmd.properties";
-	private static String key_localFolder="localFolder";
-	private static String key_defaultFs="defaultFS";
-	
+	private static String cfgProperties = "testETLCmd.properties";
+	private static String key_localFolder = "localFolder";
+	private static String key_defaultFs = "defaultFS";
+
 	private Properties p = new Properties();
-	
+
 	private String localFolder = "";
 	private FileSystem fs;
 	private String defaultFS;
 	private Configuration conf;
-	
+
 	@Before
-    public void setUp() {
-		try{
-			InputStream input = this.getClass().getClassLoader().getResourceAsStream(cfgProperties);
-			if (input!=null){
-				p.load(input);
-				localFolder = (p.getProperty(key_localFolder));
-				conf = new Configuration();
-				defaultFS = p.getProperty(key_defaultFs);
-				conf.set("fs.defaultFS", defaultFS);
-				fs = FileSystem.get(conf);
-			}else{
-				logger.error(String.format("%s not found in classpath.", cfgProperties));
+	public void setUp() throws Exception {
+		UserGroupInformation ugi = UserGroupInformation.createProxyUser("dbadmin", UserGroupInformation.getLoginUser());
+		ugi.doAs(new PrivilegedExceptionAction<Void>() {
+			public Void run() throws Exception {
+				try {
+					InputStream input = this.getClass().getClassLoader().getResourceAsStream(cfgProperties);
+					if (input != null) {
+						p.load(input);
+						localFolder = (p.getProperty(key_localFolder));
+						conf = new Configuration();
+						defaultFS = p.getProperty(key_defaultFs);
+						conf.set("fs.defaultFS", defaultFS);
+						fs = FileSystem.get(conf);
+					} else {
+						logger.error(String.format("%s not found in classpath.", cfgProperties));
+					}
+				} catch (Exception e) {
+					logger.error("", e);
+				}
+				return null;
 			}
-		}catch(Exception e){
-			logger.error("", e);
-		}
-    }
+		});
+	}
 
 	public String getLocalFolder() {
 		return localFolder;
@@ -47,12 +56,12 @@ public class TestETLCmd {
 	public FileSystem getFs() {
 		return fs;
 	}
-	
+
 	public String getDefaultFS() {
 		return defaultFS;
 	}
-	
-	public Configuration getConf(){
+
+	public Configuration getConf() {
 		return conf;
 	}
 }
