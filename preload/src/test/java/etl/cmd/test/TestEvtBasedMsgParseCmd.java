a860b5eee059f140c323db76c2b8ce0a04c1bdc8
rajashankary <rajashankar.yaligar@hpe.com>
2016-07-04 11:28:25 +0530

    Event Based message parsing: test case 5

--- preload/src/test/java/etl/cmd/test/TestEvtBasedMsgParseCmd.java
+++ preload/src/test/java/etl/cmd/test/TestEvtBasedMsgParseCmd.java
@@ -1,63 +1,100 @@
-package etl.cmd.test;
+ package etl.cmd.test;
+ 
+  import static org.junit.Assert.assertTrue;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.security.PrivilegedExceptionAction;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.NullWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+ import org.apache.hadoop.fs.FileSystem;
+  import org.apache.hadoop.fs.Path;
+  import org.apache.hadoop.io.NullWritable;
+  import org.apache.hadoop.io.Text;
+ import org.apache.hadoop.mapreduce.Job;
+ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.log4j.Logger;
-import org.junit.Test;
+  import org.junit.Test;
+  
+ import etl.engine.ETLCmd;
+  import etl.engine.InvokeMapper;
+import etl.util.Util;
+  
+  public class TestEvtBasedMsgParseCmd extends TestETLCmd{
+ 	public static final Logger logger = Logger.getLogger(TestEvtBasedMsgParseCmd.class);
+ 	public static SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+ 	
+ 	@Test
+ 	public void test1() throws IOException, InterruptedException{
+		UserGroupInformation ugi = UserGroupInformation.createProxyUser("dbadmin", UserGroupInformation.getLoginUser());
+		ugi.doAs(new PrivilegedExceptionAction<Void>() {
+			public Void run() throws Exception {
+ 		try{
+ 			String remoteCfgFolder = "/etltest/cfg/";
+ 			String remoteCsvFolder = "/etltest/msgparse/";
+ 			String remoteCsvOutputFolder = "/etltest/msgparseout/";
+ 			String csvtransProp = "hlr.msgparse.singlecmd.properties";
+ 			String localExpectedFile = "expected_file";
+ 			String[] csvFiles = new String[]{"input.hlr1.txt"};
+ 			getFs().mkdirs(new Path(remoteCfgFolder));
+ 			getFs().mkdirs(new Path(remoteCsvFolder));
+ 			getFs().delete(new Path(remoteCsvFolder), true);
+ 			getFs().copyFromLocalFile(new Path(getLocalFolder()+csvtransProp), new Path(remoteCfgFolder+csvtransProp));
+ 			for (String csvFile:csvFiles){
+ 				getFs().copyFromLocalFile(new Path(getLocalFolder()+csvFile), new Path(remoteCsvFolder+csvFile));
+ 			}
+ 			getFs().delete(new Path(remoteCsvOutputFolder), true);
+ 			//run job
+ 			getConf().set(InvokeMapper.cfgkey_cmdclassname, "etl.cmd.transform.EvtBasedMsgParseCmd");
+ 			getConf().set(InvokeMapper.cfgkey_wfid, sdf.format(new Date()));
+ 			getConf().set(InvokeMapper.cfgkey_staticconfigfile, remoteCfgFolder+csvtransProp);
+ 			Job job = Job.getInstance(getConf(), "testCsvTransformCmd");
+ 			job.setMapperClass(etl.engine.InvokeMapper.class);
+ 			job.setNumReduceTasks(0);//no reducer
+ 			job.setOutputKeyClass(Text.class);
+ 			job.setOutputValueClass(NullWritable.class);
+ 			FileInputFormat.setInputDirRecursive(job, true);
+ 			FileInputFormat.addInputPath(job, new Path(remoteCsvFolder));
+ 			FileOutputFormat.setOutputPath(job, new Path(remoteCsvOutputFolder));
+ 			job.waitForCompletion(true);
+ 			
+ 			//check results
+ 			//Get data from the Standard expected output file
+ 			ArrayList<String> expectedData=new ArrayList<String>();
+ 			String line=null;
+ 			BufferedReader br = new BufferedReader(new FileReader(getLocalFolder() + localExpectedFile));
+ 			while ((line = br.readLine()) != null) {
+ 				expectedData.add(line);  
+ 			}
+ 			br.close();
+ 			//Get data from the output parsed file
+ 			List<String> output = Util.getMROutput(getFs(), remoteCsvOutputFolder);
 
-import etl.engine.InvokeMapper;
-
-public class TestEvtBasedMsgParseCmd {
-	public static final Logger logger = Logger.getLogger(TestEvtBasedMsgParseCmd.class);
-	public static SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
-	
-	@Test
-	public void test1(){
-		try{
-			Configuration conf = new Configuration();
-			String defaultFS = "hdfs://127.0.0.1:19000";
-			conf.set("fs.defaultFS", defaultFS);
-			String remoteCfgFolder = "/etltest/cfg/";
-			String remoteCsvFolder = "/etltest/msgparse/";
-			String remoteCsvOutputFolder = "/etltest/msgparseout/";
-			FileSystem fs = FileSystem.get(conf);
-			//setup testing env
-			//String localResourceFolder = "/Users/chengyi/git/log.analysis/preload/src/test/resources/";
-			String localResourceFolder = "C:\\mydoc\\myprojects\\log.analysis\\preload\\src\\test\\resources\\";
-			String csvtransProp = "msgparse.properties";
-			String[] csvFiles = new String[]{"input.hlr1.txt"};
-			fs.mkdirs(new Path(remoteCfgFolder));
-			fs.mkdirs(new Path(remoteCsvFolder));
-			fs.delete(new Path(remoteCsvFolder), true);
-			fs.copyFromLocalFile(new Path(localResourceFolder+csvtransProp), new Path(remoteCfgFolder+csvtransProp));
-			for (String csvFile:csvFiles){
-				fs.copyFromLocalFile(new Path(localResourceFolder+csvFile), new Path(remoteCsvFolder+csvFile));
-			}
-			fs.delete(new Path(remoteCsvOutputFolder), true);
-			//run job
-			conf.set(InvokeMapper.cfgkey_cmdclassname, "etl.cmd.transform.EvtBasedMsgParseCmd");
-			conf.set(InvokeMapper.cfgkey_wfid, sdf.format(new Date()));
-			conf.set(InvokeMapper.cfgkey_staticconfigfile, remoteCfgFolder+csvtransProp);
-			Job job = Job.getInstance(conf, "testCsvTransformCmd");
-			job.setMapperClass(etl.engine.InvokeMapper.class);
-			job.setNumReduceTasks(0);//no reducer
-			job.setOutputKeyClass(Text.class);
-			job.setOutputValueClass(NullWritable.class);
-			FileInputFormat.setInputDirRecursive(job, true);
-			FileInputFormat.addInputPath(job, new Path(remoteCsvFolder));
-			FileOutputFormat.setOutputPath(job, new Path(remoteCsvOutputFolder));
-			job.waitForCompletion(true);
+ 			//check the results match the expected output
+ 			assertTrue(output.size()==expectedData.size());
+ 			assertTrue(output.containsAll(expectedData));
+ 			logger.info("The results verified succesfully.!");	
 		} catch (Exception e) {
-			logger.error("", e);
-		}
-	}
-}
+ 			logger.error("", e);
+ 		}
+ 		
+		return null;
+			}
+		});
+
+ 	}
+ }
