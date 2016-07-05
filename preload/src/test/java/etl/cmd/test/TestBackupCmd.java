3057427d5cacf63850457b1bbe6b99003d8c614f
phillipcheng <phillipchengyi@gmail.com>
2016-07-05 14:45:37 +0800

    changes the text formatting

--- preload/src/test/java/etl/cmd/test/TestBackupCmd.java
+++ preload/src/test/java/etl/cmd/test/TestBackupCmd.java
@@ -22,72 +22,72 @@ public class TestBackupCmd extends TestETLCmd{
 
 	@Test
 	public void test1() throws Exception{
-				try {
-					//
-					String dynFolder = "/test/BackupCmd/data/dynFolder1/";
-					String allFolder = "/test/BackupCmd/data/allFolder1/";
-					String wfidFolder = "/test/BackupCmd/data/wfidFolder1/";
+		try {
+			//
+			String dynFolder = "/test/BackupCmd/data/dynFolder1/";
+			String allFolder = "/test/BackupCmd/data/allFolder1/";
+			String wfidFolder = "/test/BackupCmd/data/wfidFolder1/";
 
-					String[] dynCfgFileNames = new String[]{"dynCfgFile1", "dynCfgFile2"};
-					String dfsCfgFolder = "/test/BackupCmd/cfg/";
-					String dynCfgName = "dynCfg1";
-					String staticCfgName = "backup_test1_staticCfg.properties";
-					String wfid="wfid1";
+			String[] dynCfgFileNames = new String[]{"dynCfgFile1", "dynCfgFile2"};
+			String dfsCfgFolder = "/test/BackupCmd/cfg/";
+			String dynCfgName = "dynCfg1";
+			String staticCfgName = "backup_test1_staticCfg.properties";
+			String wfid="wfid1";
 
-					String[] dynFiles = new String[]{"dynCfgFile1", "dynCfgFile2", "dynCfgFile3"};
-					String[] allFiles = new String[]{"all1", "all2"};
-					String[] wfidFiles = new String[]{wfid+"a", wfid+"b", "a"};
+			String[] dynFiles = new String[]{"dynCfgFile1", "dynCfgFile2", "dynCfgFile3"};
+			String[] allFiles = new String[]{"all1", "all2"};
+			String[] wfidFiles = new String[]{wfid+"a", wfid+"b", "a"};
 
-					String localFile = "backup_test1_data";
-					//values should be in the configuration file
-					String dynKey = "dynFiles";
-					String historyFolder = "/test/datahistory/";
-					//generate all the data files
-					getFs().delete(new Path(dynFolder), true);
-					getFs().delete(new Path(allFolder), true);
-					getFs().delete(new Path(wfidFolder), true);
-					for (String dynFile: dynFiles){
-						getFs().copyFromLocalFile(new Path(getLocalFolder() + localFile), new Path(dynFolder + dynFile));
-					}
-					for (String allFile: allFiles){
-						getFs().copyFromLocalFile(new Path(getLocalFolder() + localFile), new Path(allFolder + allFile));
-					}
-					for (String wfidFile: wfidFiles){
-						getFs().copyFromLocalFile(new Path(getLocalFolder() + localFile), new Path(wfidFolder + wfidFile));
-					}
+			String localFile = "backup_test1_data";
+			//values should be in the configuration file
+			String dynKey = "dynFiles";
+			String historyFolder = "/test/datahistory/";
+			//generate all the data files
+			getFs().delete(new Path(dynFolder), true);
+			getFs().delete(new Path(allFolder), true);
+			getFs().delete(new Path(wfidFolder), true);
+			for (String dynFile: dynFiles){
+				getFs().copyFromLocalFile(new Path(getLocalFolder() + localFile), new Path(dynFolder + dynFile));
+			}
+			for (String allFile: allFiles){
+				getFs().copyFromLocalFile(new Path(getLocalFolder() + localFile), new Path(allFolder + allFile));
+			}
+			for (String wfidFile: wfidFiles){
+				getFs().copyFromLocalFile(new Path(getLocalFolder() + localFile), new Path(wfidFolder + wfidFile));
+			}
 
-					//generate local dyn conf file
-					Map<String, List<String>> dynCfgValues = new HashMap<String, List<String>>();
-					dynCfgValues.put(dynKey, Arrays.asList(dynCfgFileNames));
-					Util.toLocalJsonFile(getLocalFolder() + dynCfgName, dynCfgValues);
+			//generate local dyn conf file
+			Map<String, List<String>> dynCfgValues = new HashMap<String, List<String>>();
+			dynCfgValues.put(dynKey, Arrays.asList(dynCfgFileNames));
+			Util.toLocalJsonFile(getLocalFolder() + dynCfgName, dynCfgValues);
 
-					//add the local conf file to dfs
-					getFs().copyFromLocalFile(new Path(getLocalFolder() + staticCfgName), new Path(dfsCfgFolder + staticCfgName));
-					getFs().copyFromLocalFile(new Path(getLocalFolder() + dynCfgName), new Path(dfsCfgFolder + dynCfgName));
+			//add the local conf file to dfs
+			getFs().copyFromLocalFile(new Path(getLocalFolder() + staticCfgName), new Path(dfsCfgFolder + staticCfgName));
+			getFs().copyFromLocalFile(new Path(getLocalFolder() + dynCfgName), new Path(dfsCfgFolder + dynCfgName));
 
-					//run cmd
-					BackupCmd cmd = new BackupCmd(wfid, dfsCfgFolder + staticCfgName, dfsCfgFolder + dynCfgName, null, getDefaultFS());
-					cmd.process(0, null, null);
+			//run cmd
+			BackupCmd cmd = new BackupCmd(wfid, dfsCfgFolder + staticCfgName, dfsCfgFolder + dynCfgName, null, getDefaultFS());
+			cmd.process(0, null, null);
 
-					//check results
-					//dynFolder should only contains 1 file
-					List<String> flist;
-					flist = Util.listDfsFile(getFs(), dynFolder);
-					assertTrue(flist.size()==1);
-					assertTrue(flist.contains("dynCfgFile3"));
-					//allFolder should be empty
-					flist = Util.listDfsFile(getFs(), allFolder);
-					assertTrue(flist.size()==0);
-					//wfidFolder should have only 1 file
-					flist = Util.listDfsFile(getFs(), wfidFolder);
-					assertTrue(flist.size()==1);
-					assertTrue(flist.contains("a"));
-					//check the zip file
-					flist = Util.listDfsFile(getFs(), historyFolder);
-					assertTrue(flist.contains(wfid+".zip"));
-				} catch (Exception e) {
-					logger.error("Exception occured ", e);
-				}
+			//check results
+			//dynFolder should only contains 1 file
+			List<String> flist;
+			flist = Util.listDfsFile(getFs(), dynFolder);
+			assertTrue(flist.size()==1);
+			assertTrue(flist.contains("dynCfgFile3"));
+			//allFolder should be empty
+			flist = Util.listDfsFile(getFs(), allFolder);
+			assertTrue(flist.size()==0);
+			//wfidFolder should have only 1 file
+			flist = Util.listDfsFile(getFs(), wfidFolder);
+			assertTrue(flist.size()==1);
+			assertTrue(flist.contains("a"));
+			//check the zip file
+			flist = Util.listDfsFile(getFs(), historyFolder);
+			assertTrue(flist.contains(wfid+".zip"));
+		} catch (Exception e) {
+			logger.error("Exception occured ", e);
+		}
 	}
 
 	@Test
--- preload/src/test/java/etl/cmd/test/TestShellCmd.java
+++ preload/src/test/java/etl/cmd/test/TestShellCmd.java
@@ -5,16 +5,9 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.security.PrivilegedExceptionAction;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.commons.io.FileUtils;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
@@ -35,54 +28,53 @@ public class TestShellCmd extends TestETLCmd {
 
 	@Test
 	public void testShellCmd() throws Exception {
-					try{
-					String remoteCfgFolder = "/pde/cfg";
-					String remoteOutputFolder="/pde/output";
-					//setup testing env
-					String shellProp = "shellCmd1.properties";
-					String executableFile = "copyFile.bat";
-					String executableDir = "C:\\Users";
-					getFs().delete(new Path(remoteOutputFolder), true);
-					getFs().copyFromLocalFile(new Path(getLocalFolder()+shellProp), new Path(remoteCfgFolder+shellProp));
-					//run job  
-					FileUtils.copyFileToDirectory(new File(getLocalFolder()+executableFile), new File(executableDir));
-					getConf().set(InvokeMapper.cfgkey_cmdclassname, "etl.cmd.ShellCmd");
-					getConf().set(InvokeMapper.cfgkey_wfid, sdf.format(new Date()));
-					getConf().set(InvokeMapper.cfgkey_staticconfigfile, remoteCfgFolder+shellProp);
-					Job job = Job.getInstance(getConf(), "testShellCmd");
-					job.setMapperClass(etl.engine.InvokeMapper.class);
-					job.setNumReduceTasks(0);//no reducer
-					job.setOutputKeyClass(Text.class);
-					job.setOutputValueClass(NullWritable.class);
-					FileInputFormat.setInputDirRecursive(job, true);
-					FileInputFormat.addInputPath(job, new Path(remoteCfgFolder+shellProp));
-					FileOutputFormat.setOutputPath(job, new Path(remoteOutputFolder));
-					job.waitForCompletion(true);
+		try{
+			String remoteCfgFolder = "/pde/cfg";
+			String remoteOutputFolder="/pde/output";
+			//setup testing env
+			String shellProp = "shellCmd1.properties";
+			String executableFile = "copyFile.bat";
+			String executableDir = "C:\\Users";
+			getFs().delete(new Path(remoteOutputFolder), true);
+			getFs().copyFromLocalFile(new Path(getLocalFolder()+shellProp), new Path(remoteCfgFolder+shellProp));
+			//run job  
+			FileUtils.copyFileToDirectory(new File(getLocalFolder()+executableFile), new File(executableDir));
+			getConf().set(InvokeMapper.cfgkey_cmdclassname, "etl.cmd.ShellCmd");
+			getConf().set(InvokeMapper.cfgkey_wfid, sdf.format(new Date()));
+			getConf().set(InvokeMapper.cfgkey_staticconfigfile, remoteCfgFolder+shellProp);
+			Job job = Job.getInstance(getConf(), "testShellCmd");
+			job.setMapperClass(etl.engine.InvokeMapper.class);
+			job.setNumReduceTasks(0);//no reducer
+			job.setOutputKeyClass(Text.class);
+			job.setOutputValueClass(NullWritable.class);
+			FileInputFormat.setInputDirRecursive(job, true);
+			FileInputFormat.addInputPath(job, new Path(remoteCfgFolder+shellProp));
+			FileOutputFormat.setOutputPath(job, new Path(remoteOutputFolder));
+			job.waitForCompletion(true);
 
-					//check results
-					PropertiesConfiguration pc = Util.getPropertiesConfigFromDfs(getFs(), remoteCfgFolder + shellProp);
-					File sourceDir = new File(pc.getString("srcfolder"));
-					assertTrue(sourceDir.exists());
+			//check results
+			PropertiesConfiguration pc = Util.getPropertiesConfigFromDfs(getFs(), remoteCfgFolder + shellProp);
+			File sourceDir = new File(pc.getString("srcfolder"));
+			assertTrue(sourceDir.exists());
 
-					File destDir = new File(pc.getString("destfolder"));
-					assertTrue(destDir.exists());
+			File destDir = new File(pc.getString("destfolder"));
+			assertTrue(destDir.exists());
 
-					File[] srcFiles= sourceDir.listFiles();
-					File[] destFiles= sourceDir.listFiles();
-					assert(srcFiles.length==destFiles.length);
+			File[] srcFiles= sourceDir.listFiles();
+			File[] destFiles= sourceDir.listFiles();
+			assert(srcFiles.length==destFiles.length);
 
-					boolean result=true;
-					for (int i = 0; i < srcFiles.length; i++) {
-						if(!srcFiles[i].getName().equals(destFiles[i].getName()))
-						{
-							result=false;
-						}
-					}
-					assertTrue(result);
-					logger.info("Results verified successfully ..!!");
-				} catch (Exception e) {
-					logger.error("", e);
+			boolean result=true;
+			for (int i = 0; i < srcFiles.length; i++) {
+				if(!srcFiles[i].getName().equals(destFiles[i].getName())) {
+					result=false;
 				}
+			}
+			assertTrue(result);
+			logger.info("Results verified successfully ..!!");
+		} catch (Exception e) {
+			logger.error("", e);
+		}
 	}
 	
 	@Test
