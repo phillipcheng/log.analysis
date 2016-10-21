8f221d766efb01335869b9c4ee3440cb79d260bc
hpe001 <hpe001@hpe.com>
2016-10-21 20:10:09 +0800

    update to LocalPropertiesUtil

--- dashview/src/main/java/dv/util/ConfigManager.java
+++ dashview/src/main/java/dv/util/ConfigManager.java
@@ -8,12 +8,14 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import dv.tableau.bl.TableauBLImpl;
+import etl.util.LocalPropertiesUtil;
 import etl.util.PropertiesUtil;
 
 /**
@@ -25,18 +27,11 @@ public class ConfigManager {
 	public static final Logger logger = LogManager.getLogger(ConfigManager.class);
 	public static Map getProperties() {
 		Map<String, String> returnMap = new HashMap<String, String>();
-		logger.info(ConfigManager.class.getResource("/").getPath());;
-		InputStream inputStream = new ConfigManager().getClass().getClassLoader()
-				.getResourceAsStream("config/config.properties");
-		Properties prop = new Properties();
-		try {
-			prop.load(inputStream);
-			returnMap.put("proxyHost", prop.getProperty("proxyHost"));
-			returnMap.put("proxyPort", prop.getProperty("proxyPort"));
-			returnMap.put("tableauip", prop.getProperty("tableauip"));
-		} catch (IOException e1) {
-			logger.error("", e1);
-		}
-		return returnMap;
+		PropertiesConfiguration pc = LocalPropertiesUtil.getPropertiesConfig("config/config.properties");
+		returnMap.put("proxyHost", pc.getString("proxyHost"));
+		returnMap.put("proxyPort", pc.getString("proxyPort"));
+		returnMap.put("tableauip", pc.getString("tableauip"));
+		logger.info(returnMap);
+		return returnMap; 
 	}
 }
