94f6f472e9b6cd5846a6869983f211bd63f17b8f
hanzac <hanzac@gmail.com>
2016-10-27 13:06:53 +0800

    Update the test case

--- bdap-engine/src/test/java/etl/cmd/test/TestUnpackCmd.java
+++ bdap-engine/src/test/java/etl/cmd/test/TestUnpackCmd.java
@@ -21,47 +21,27 @@ public class TestUnpackCmd extends TestETLCmd {
 		return "unpack/";
 	}
 
-	private void testUnpack() throws Exception {
-		try {
-			String remoteCsvInputFolder = "/etltest/unpack/input/";
-			String remoteCsvOutputFolder = "/etltest/unpack/output/";
-			String unpackProp = "unpack.properties";
-			String[] csvFiles = new String[]{"20160409_BB1.zip", "test.csv"};
-
-			getFs().delete(new Path("/etltest/unpack/output/20160409_BB1"), true);
-			
-			List<String> output = super.mapTest(remoteCsvInputFolder, remoteCsvOutputFolder, unpackProp, csvFiles, testCmdClass, true);
-			logger.info("Output is:"+output);
-			
-			List<String> fl = HdfsUtil.listDfsFile(getFs(), "/etltest/unpack/output/20160409_BB1/01");
-			//assert
-			logger.info(fl);
-			assertTrue(fl.contains("A20160409.0000-20160409.0100_E_000000000_262216704_F4D9FB75EA47_13.csv"));
-			
-			fl = HdfsUtil.listDfsFile(getFs(), "/etltest/unpack/output/20160409_BB1");
-			logger.info(fl);
-			assertTrue(fl.contains("test.txt"));
-			assertTrue(!fl.contains("test.dat"));
-			
-		} catch (Exception e) {
-			logger.error("", e);
-			assertTrue(false);
-		}
-	}
-	
 	@Test
-	public void test() throws Exception {
-		if (getDefaultFS().contains("127.0.0.1")){
-			testUnpack();
-		}else{
-			UserGroupInformation ugi = UserGroupInformation.createProxyUser("player", UserGroupInformation.getLoginUser());
-			ugi.doAs(new PrivilegedExceptionAction<Void>() {
-				public Void run() throws Exception {
-					testUnpack();
-					return null;
-				}
-			});
-		}
+	public void testUnpack() throws Exception {
+		String remoteCsvInputFolder = "/etltest/unpack/input/";
+		String remoteCsvOutputFolder = "/etltest/unpack/output/";
+		String unpackProp = "unpack.properties";
+		String[] csvFiles = new String[]{"20160409_BB1.zip", "test.csv"};
+
+		getFs().delete(new Path("/etltest/unpack/output/20160409_BB1"), true);
+		
+		List<String> output = super.mapTest(remoteCsvInputFolder, remoteCsvOutputFolder, unpackProp, csvFiles, testCmdClass, true);
+		logger.info("Output is:"+output);
+		
+		List<String> fl = HdfsUtil.listDfsFile(getFs(), "/etltest/unpack/output/20160409_BB1/01");
+		//assert
+		logger.info(fl);
+		assertTrue(fl.contains("A20160409.0000-20160409.0100_E_000000000_262216704_F4D9FB75EA47_13.csv"));
+		
+		fl = HdfsUtil.listDfsFile(getFs(), "/etltest/unpack/output/20160409_BB1");
+		logger.info(fl);
+		assertTrue(fl.contains("test.txt"));
+		assertTrue(!fl.contains("test.dat"));
 	}
 	
 }
