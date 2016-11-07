6df432917634ed9192e8318d161ac7a66a2faa49
Zhu, Xiang <xiang.zhu2@hpe.com>
2016-11-07 19:57:04 +0800

    modify CsvTransposeCmd

--- bdap-engine/src/main/java/etl/cmd/CsvTransposeCmd.java
+++ bdap-engine/src/main/java/etl/cmd/CsvTransposeCmd.java
@@ -1,6 +1,5 @@
 package etl.cmd;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,7 +10,6 @@ import javax.script.CompiledScript;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -19,7 +17,6 @@ import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import bdap.util.HdfsUtil;
 import etl.cmd.transform.ColOp;
 import etl.engine.ETLCmd;
 import etl.engine.MRMode;
@@ -46,39 +43,46 @@ public class CsvTransposeCmd extends SchemaFileETLCmd {
 	
 	private static final Logger logger = LogManager.getLogger(CsvTransposeCmd.class);
 	
-	private static final String VAR_NAME_SPLITED_TABLE_NAME="splitedTableName";
+	//Stor the origin configured table name or splited table name
+	public static final String VAR_NAME_ORIGIN_TABLE_NAME="originTableName";
 	
 	//cfgkey	
 	public static final String cfgkey_skip_header = "skip.header";
 	public static final String cfgkey_with_trailing_delimiter = "with.trailing.delimiter";	
-	public static final String cfgkey_transpose_schema_file = "transpose.schema.file";	
 	
-	//Besides the row transpose to column, output specified raw group columns 
-	public static final String cfgkey_rows_fields = "row.fields";
+	//Group of columns output
+	public static final String cfgkey_group_fields = "group.fields";
 	
-	//field.name.keys, field.value.keys & field.name.keys.schema.type they are 1:1:1 configured
-	//define which columns value will transpose to field name
+	//column.name.fields  & column.name.fields appear in pair to indicate which column's value need to transfer to column name and which column's value need to be filled into the new column
 	public static final String cfgkey_column_name_fields = "column.name.fields";
-	//define how many possible field names
-	public static final String cfgkey_column_name_field_schema_types = "column.name.field.schema.types";
-	//define which columns value will transpose to transposed field's value
-	public static final String cfgkey_column_value_field = "column.value.fields";
+	public static final String cfgkey_column_value_fields = "column.value.fields";
 	
-	//used for the mapping find table type list
+	//Used only when split.table.fields is not used, to specify the output result table schema
+	public static final String cfgkey_table_name = "table.name";
+	
+	//Use specified fields as table name
 	public static final String cfgkey_split_table_fields = "split.table.fields";
 	
+
+	public static final String cfgkey_table_name_mapping_exp = "table.name.mapping.exp";
+
+	public static final String cfgkey_table_fields_mapping_exp = "table.fields.mapping.exp";
+	
 	//used to output the result into different files, if not configuration, all expose into single file
-	public static final String cfgkey_table_name_exp = "table.name.exp";
+	public static final String cfgkey_output_filename_exp = "output.filename.exp";
 
 	private boolean skipHeader = false;
 	private boolean withTrailingDelimiter = false;
-	private List<IdxRange> rowFieldList=new ArrayList<IdxRange>();
-	private List<IdxRange> splitTableFieldList=new ArrayList<IdxRange>();
-	private List<Integer> columnNameFieldList=new ArrayList<Integer>();
-	private List<String> noSplitedTypeColumnNameFieldSchemaTypeList=new ArrayList<String>();
-	private List<Integer> columnValueFieldList=new ArrayList<Integer>();	
-	private CompiledScript tableNameCS=null;
-	private TransposeSchema transposeSchema=null;
+	private List<IdxRange> groupFieldList=new ArrayList<IdxRange>();	
+	private String tableName;
+	private List<IdxRange> splitTableFieldIdxList=new ArrayList<IdxRange>();	
+	private List<Integer> columnNameFieldIdxList=new ArrayList<Integer>();	
+	private List<Integer> columnValueFieldIdxList=new ArrayList<Integer>();		
+	private boolean isSplitTable=false;
+
+	private CompiledScript outputFileNameCS=null;
+	private CompiledScript tableNameMappingCS=null;
+	private CompiledScript tableFieldsMappingCS=null;
 	
 	public CsvTransposeCmd(){
 		super();
@@ -98,54 +102,73 @@ public class CsvTransposeCmd extends SchemaFileETLCmd {
 		setMrMode(MRMode.line);
 		this.skipHeader =getCfgBoolean(cfgkey_skip_header, false);
 		this.withTrailingDelimiter=getCfgBoolean(cfgkey_with_trailing_delimiter, false);
-		this.rowFieldList=IdxRange.parseString(getCfgString(cfgkey_rows_fields, null));
-		this.splitTableFieldList=IdxRange.parseString(getCfgString(cfgkey_split_table_fields, null));
+		this.groupFieldList=IdxRange.parseString(getCfgString(cfgkey_group_fields, null));
 		
-		String transposeSchemaFile=getCfgString(cfgkey_transpose_schema_file, null);
-		transposeSchema=loadTransposeSchema(transposeSchemaFile);		
-		logger.info("Transpose Schema:{}", transposeSchema);
-		
-		String tableNameExp=super.getCfgString(cfgkey_table_name_exp, null);
-		if(tableNameExp!=null){
-			tableNameCS=ScriptEngineUtil.compileScript(tableNameExp);
-		}	
-		
-		//Read field name keys
-		String[] columnNameFieldsArray=super.getCfgStringArray(cfgkey_column_name_fields);
-		if(columnNameFieldsArray!=null){
-			for (String columnNameField :columnNameFieldsArray) {
-				this.columnNameFieldList.add(Integer.parseInt(columnNameField));
-			}
-		}		
-		
-		//Read field name key schema types
-		String[] columnNameFieldSchemaTypesArray=super.getCfgStringArray(cfgkey_column_name_field_schema_types);
-		if(columnNameFieldSchemaTypesArray!=null){
-			this.noSplitedTypeColumnNameFieldSchemaTypeList.addAll(Arrays.asList(columnNameFieldSchemaTypesArray));
+		String outputFilenameExp=super.getCfgString(cfgkey_output_filename_exp, null);
+		if(outputFilenameExp!=null){
+			outputFileNameCS=ScriptEngineUtil.compileScript(outputFilenameExp);
 		}
 		
-		//Read field value keys
-		String[] columnValueFieldsArray=super.getCfgStringArray(cfgkey_column_value_field);
-		if(columnValueFieldsArray!=null){
-			for (String columnValueField :columnValueFieldsArray) {
-				this.columnValueFieldList.add(Integer.parseInt(columnValueField));
+		//Read column name field index
+		String[] columnNameFieldIdxArray = super.getCfgStringArray(cfgkey_column_name_fields);
+		if (columnNameFieldIdxArray != null) {
+			for (String index : columnNameFieldIdxArray) {
+				columnNameFieldIdxList.add(Integer.parseInt(index));
 			}
 		}
+		if(columnNameFieldIdxArray==null || columnNameFieldIdxArray.length==0){
+			throw new IllegalArgumentException(String.format("column.name.fields parameter is mandatory"));
+		}
 		
-//		if(columnNameFieldList.size()==0 || columnValueFieldList.size()==0 || noSplitedTypeColumnNameFieldSchemaTypeList.size()==0){
-//			throw new IllegalArgumentException(String.format("field name keys, field values and field name key schema type are mandatory"));
-//		}
+		//Read column value field index
+		String[] columnValueFieldIdxArray = super.getCfgStringArray(cfgkey_column_value_fields);
+		if (columnValueFieldIdxArray != null) {
+			for (String index : columnValueFieldIdxArray) {
+				this.columnValueFieldIdxList.add(Integer.parseInt(index));
+			}
+		}
+		if(columnValueFieldIdxArray==null || columnValueFieldIdxArray.length==0){
+			throw new IllegalArgumentException(String.format("column.value.fields parameter is mandatory"));
+		}
 		
-//		
-//		if ((columnNameFieldList.size()!= columnValueFieldList.size()) ||  (columnNameFieldList.size()!= noSplitedTypeColumnNameFieldSchemaTypeList.size())){
-//			throw new IllegalArgumentException(String.format("The length among field name keys, field values and field name key schema type must be same"));
-//		}
-	
+		//Validate the size of column.name.fields and column.value.fields must same
+		if(columnValueFieldIdxArray.length!=columnNameFieldIdxArray.length){
+			throw new IllegalArgumentException(String.format("column.name.fields and column.value.fields' value list must be in pair!"));
+		}
+		
+		//Read column name field table		
+		String splitTableFields=getCfgString(cfgkey_split_table_fields, null);
+		
+		if(splitTableFields!=null){
+			this.isSplitTable=true;
+			this.splitTableFieldIdxList=IdxRange.parseString(splitTableFields);
+		}else{
+			this.tableName = super.getCfgString(cfgkey_table_name,null);
+			
+//			if(this.tableName==null){
+//				throw new IllegalArgumentException(String.format("Eithr of column.name.field.tables or split.table.fields is required"));
+//			}
+			
+		}
+		
+		//Read table name mapping expression
+		String tableNameMappingExp=this.getCfgString(cfgkey_table_name_mapping_exp, null);
+		if(tableNameMappingExp!=null){
+			tableNameMappingCS=ScriptEngineUtil.compileScript(tableNameMappingExp);
+		}
+		
+		//Read table -> fields mapping expression
+		String tableFieldsMappingExp = this.getCfgString(cfgkey_table_fields_mapping_exp, null);
+		if (tableFieldsMappingExp != null) {
+			tableFieldsMappingCS = ScriptEngineUtil.compileScript(tableFieldsMappingExp);
+		}else{
+			throw new IllegalArgumentException(String.format("table.fields.mapping.exp is mandatory"));
+		}
 	}
 	
 	@Override
 	public Map<String, Object> mapProcess(long offset, String row, Mapper<LongWritable, Text, Text, Text>.Context context) throws Exception {
-		//Read file name
+		//Read filename
 		String inputFileName = ((FileSplit) context.getInputSplit()).getPath().getName();
 		getSystemVariables().put(VAR_NAME_FILE_NAME, inputFileName);
 		
@@ -164,55 +187,110 @@ public class CsvTransposeCmd extends SchemaFileETLCmd {
 		}
 		getSystemVariables().put(ColOp.VAR_NAME_FIELDS, fields);
 		
-		//Get table name
-		String splitedTableName=buildString(csvRecord,splitTableFieldList,"_");
-		getSystemVariables().put(VAR_NAME_SPLITED_TABLE_NAME, splitedTableName);
-		String tableName=splitedTableName;
-		if(this.tableNameCS!=null){
-			tableName=ScriptEngineUtil.eval(tableNameCS, super.getSystemVariables());
+		//Calculate table name
+		String originTableName;
+		String realTableName;
+		if(isSplitTable){
+			originTableName=buildString(csvRecord,splitTableFieldIdxList,"_");
+		}else{
+			originTableName=this.tableName;
+		}
+		realTableName=originTableName;
+		getSystemVariables().put(VAR_NAME_TABLE_NAME, originTableName);
+		getSystemVariables().put(VAR_NAME_ORIGIN_TABLE_NAME, originTableName);
+		if(tableNameMappingCS!=null){
+			realTableName=ScriptEngineUtil.eval(tableNameMappingCS, super.getSystemVariables());
+			getSystemVariables().put(VAR_NAME_TABLE_NAME, originTableName);
+		}
+		
+		//Calculate output filename
+		String outputFileName=realTableName;
+		if(this.outputFileNameCS!=null){
+			outputFileName=ScriptEngineUtil.eval(outputFileNameCS, super.getSystemVariables());
 		}		
 		
-		//Get row Keys
-		String rows=buildString(csvRecord,rowFieldList,",");
+		//Get grouped columns
+		String groupedColumns=buildString(csvRecord,groupFieldList,",");
+		
+		//Calculate number of grouped columns
+		int numberOfGroupedColumns=0;
+		for(IdxRange idxRange:groupFieldList){
+			int start=idxRange.getStart();
+			int end=idxRange.getEnd();
+			if(end==-1){
+				end=csvRecord.size()-1;
+			}
+			numberOfGroupedColumns=numberOfGroupedColumns+(end-start+1);
+		}
+		
+		//Read table field
+		List<List<String>> tableFieldGroupList=new ArrayList<List<String>>();
+		int numberOfTransposeColumns=0;
+		String fieldsStr=ScriptEngineUtil.eval(tableFieldsMappingCS, super.getSystemVariables());
+		if(fieldsStr==null || fieldsStr.trim().isEmpty()){
+			logger.info("Cannot find transpose columns definition, drop row:{}",row);
+			return null;
+		}
+		
+		String[] fieldGroupArray=fieldsStr.split(";");
+		if(fieldGroupArray!=null && fieldGroupArray.length>0){
+			for(String fieldGroup:fieldGroupArray){
+				String[] fieldArray=fieldGroup.split(",");
+				if(fieldArray!=null){
+					numberOfTransposeColumns=numberOfTransposeColumns+fieldArray.length;
+					tableFieldGroupList.add(Arrays.asList(fieldArray));
+				}
+			}
+		}
+		
 		
 		//Transpose
-		List<String> columnNameFieldSchemaTypeList=noSplitedTypeColumnNameFieldSchemaTypeList;
-		if(this.splitTableFieldList!=null && splitTableFieldList.size()>0){
-			columnNameFieldSchemaTypeList=this.transposeSchema.getTableMap().get(splitedTableName);
-		}
-		
-		if(columnNameFieldSchemaTypeList==null || columnNameFieldSchemaTypeList.isEmpty()){
-			logger.info("Column name field schema type is not defined, skip row:{}", row);
-			return null;
-		}			
-		
-		List<String> transposeList=new ArrayList<String>();
-		for(int idx=0;idx<columnNameFieldSchemaTypeList.size();idx++){
-			//build new columns
-			List<String> colNameList=this.transposeSchema.getTypeMap().get(columnNameFieldSchemaTypeList.get(idx));	
-			List<String> subTransposeList=new ArrayList<String>(colNameList.size());
-			for(int i=0;i<colNameList.size();i++) subTransposeList.add("");
+		List<String> transposeList=new ArrayList<String>(numberOfTransposeColumns);
+		int fieldGroupIdxOffset=0;
+		for(int i=0;i<numberOfTransposeColumns;i++) transposeList.add("");
+		for(int idx=0;idx<this.columnNameFieldIdxList.size();idx++){
+			int columnNameFieldIdx=this.columnNameFieldIdxList.get(idx);
+			int columnValueFieldIdx=this.columnValueFieldIdxList.get(idx);
+			//Read & validate column name and value
+			if(columnNameFieldIdx>=csvRecord.size()){
+				logger.info("The column.name.field:{} is greater than number of record columns:{}, drop row:{}",new Object[]{columnNameFieldIdx, csvRecord.size(), row});
+				return null;
+			}
 			
-			//values to specified location of columns
-			String colName=csvRecord.get(columnNameFieldList.get(idx));
-			String colValue=csvRecord.get(columnValueFieldList.get(idx));
+			if(columnValueFieldIdx>=csvRecord.size()){
+				logger.info("The column.value.field:{} is greater than number of record columns:{}, drop row:{}",new Object[]{columnValueFieldIdx, csvRecord.size(), row});
+				return null;
+			}		
+			String colName=csvRecord.get(columnNameFieldIdx);
+			String colValue=csvRecord.get(columnValueFieldIdx);
 			
-			int indexOfColName=colNameList.indexOf(colName);
+			//Locate the column name position in the fields
+			int indexOfColName=-1;
+			List<String> fieldList=tableFieldGroupList.get(idx);
+			for(int i=0;i<fieldList.size();i++){
+				if(fieldList.get(i).equalsIgnoreCase(colName)){
+					indexOfColName=i;
+					break;
+				}
+			}
 			if(indexOfColName==-1){
-				logger.warn("Drop row:{} as cannot find releated column name in schema", row);
-				continue;
-			}			
-			subTransposeList.set(indexOfColName, colValue);
+				logger.info("Cannot find column:{} in table, drop row:{}",colName, row);
+				return null;
+			}
 			
-			//merge the schema type into full transposeList
-			transposeList.addAll(subTransposeList);
-		}
+			//Transpose
+			transposeList.set(fieldGroupIdxOffset+indexOfColName, colValue);	
+			
+			fieldGroupIdxOffset=fieldGroupIdxOffset+fieldList.size();
+		}		
 		
-		//key format
-		//  talbename,rows
-		//value format
-		//  schemaType1Value1, schemaType1Value2,,,,,schemaType1Valuen,schemaType2Value1, schemaType2Value2,,,,,schemaType2Valuen
-		context.write(new Text(tableName+","+rows), new Text(String.join(",",transposeList)));
+/*		Output 
+		key format
+		  table name,rows
+		value format
+		  value1,value2,,,,,valueN
+*/	
+		context.write(new Text(outputFileName+","+groupedColumns), new Text(String.join(",",transposeList)));
 		
 		return null;
 	}
@@ -269,57 +347,5 @@ public class CsvTransposeCmd extends SchemaFileETLCmd {
 			ret.add(new String[]{keyValue, String.join(",", recordMerged), ETLCmd.SINGLE_TABLE});
 		}
 		return ret;
-	}
-	
-	private TransposeSchema loadTransposeSchema(String transposeSchemaFile){
-		Path schemaFilePath = new Path(transposeSchemaFile);
-		try {
-			if (fs.exists(schemaFilePath)){
-				return (TransposeSchema) HdfsUtil.fromDfsJsonFile(fs, transposeSchemaFile, TransposeSchema.class);
-			}else{
-				return new TransposeSchema();
-			}
-		} catch (IOException e) {
-			logger.error("", e);
-			return new TransposeSchema();
-		}
-	}
-	
-	/**
-	 * 
-	 * @author zhuxiang
-	 *  
-	 */
-	public static class TransposeSchema{
-		//table name and the column name field schema type mapping
-		private Map<String, List<String>> tableMap;
-		
-		//type definition
-		private Map<String, List<String>> typeMap;
-		
-		
-		public Map<String, List<String>> getTableMap() {
-			return tableMap;
-		}
-		public void setTableMap(Map<String, List<String>> tableMap) {
-			this.tableMap = tableMap;
-		}
-		
-		public Map<String, List<String>> getTypeMap() {
-			return typeMap;
-		}
-		public void setTypeMap(Map<String, List<String>> typeMap) {
-			this.typeMap = typeMap;
-		}
-		@Override
-		public String toString() {
-			StringBuilder builder = new StringBuilder();
-			builder.append("TransposeSchema [tableMap=");
-			builder.append(tableMap);
-			builder.append(", typeMap=");
-			builder.append(typeMap);
-			builder.append("]");
-			return builder.toString();
-		}
 	}
 }
--- bdap-engine/src/test/java/etl/cmd/test/TestCsvTransposeCmd.java
+++ bdap-engine/src/test/java/etl/cmd/test/TestCsvTransposeCmd.java
@@ -1,23 +1,15 @@
 package etl.cmd.test;
 
-import java.util.Arrays;
-import java.util.HashMap;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.util.List;
-import java.util.Map;
 
 import org.apache.hadoop.fs.Path;
 //log4j2
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-
-import etl.cmd.CsvTransposeCmd.TransposeSchema;
-
-import static org.junit.Assert.*;
 
 public class TestCsvTransposeCmd extends TestETLCmd {
 	public static final Logger logger = LogManager.getLogger(TestCsvTransposeCmd.class);
@@ -27,33 +19,12 @@ public class TestCsvTransposeCmd extends TestETLCmd {
 		return "csvtranspose/";
 	}
 	
-	@Test
-	public void testPDEStat() throws Exception {
-		String remoteCsvFolder = "/etltest/csvtranspose/data/";
-		String remoteCsvOutputFolder = "/etltest/csvtransposeoutput/";
-		String csvtransProp = "csvTransposePDEStat.properties";
-		String[] csvFiles = new String[] {"STAT1027.Small_AJ2"};		
-		String schemaFolder = "/etltest/csvtranspose/cfg/"; //hardcoded in the properties
-		String schemaFile = "pdeStatSchema.txt";
-		getFs().copyFromLocalFile(false, true, new Path(this.getLocalFolder()+schemaFile), new Path(schemaFolder+schemaFile));
-		
-		List<String> output = super.mrTest(remoteCsvFolder, remoteCsvOutputFolder, csvtransProp, csvFiles, testCmdClass, false);
-		logger.info("Output is:\n{}",String.join("\n", output));
-		
-		assertEquals(15,output.size());
-		assertTrue(output.contains("20161027,0000,016,CTXKEY,0000000073,,,,,0000000007,,,"));
-
-	}
-	
-	@Test
+//	@Test
 	public void testSingleColumn() throws Exception {
-		String remoteCsvFolder = "/etltest/csvtranspose/data/";
-		String remoteCsvOutputFolder = "/etltest/csvtransposeoutput/";
-		String csvtransProp = "csvTransposeSimple.properties";
-		String[] csvFiles = new String[] {"csvTransposeSimpleData.csv"};		
-		String schemaFolder = "/etltest/csvtranspose/cfg/"; //hardcoded in the properties
-		String schemaFile = "schemaSimple.txt";
-		getFs().copyFromLocalFile(false, true, new Path(this.getLocalFolder()+schemaFile), new Path(schemaFolder+schemaFile));
+		String remoteCsvFolder = "/etltest/csvtranspose/input/";
+		String remoteCsvOutputFolder = "/etltest/csvtranspose/output/";
+		String csvtransProp = "singleColumn.properties";
+		String[] csvFiles = new String[] {"singleColumnData.csv"};
 		
 		List<String> output = super.mrTest(remoteCsvFolder, remoteCsvOutputFolder, csvtransProp, csvFiles, testCmdClass, false);
 		logger.info("Output is:\n{}",String.join("\n", output));
@@ -63,44 +34,39 @@ public class TestCsvTransposeCmd extends TestETLCmd {
 		assertTrue(output.contains("name3,,F,C"));
 	}
 	
+//	@Test
+	public void testMultipleColumns() throws Exception {
+		String remoteCsvFolder = "/etltest/csvtranspose/input/";
+		String remoteCsvOutputFolder = "/etltest/csvtranspose/output/";
+		String csvtransProp = "multipleColumns.properties";
+		String[] csvFiles = new String[] {"multipleColumnData.csv"};
+		
+		List<String> output = super.mrTest(remoteCsvFolder, remoteCsvOutputFolder, csvtransProp, csvFiles, testCmdClass, false);
+		logger.info("Output is:\n{}",String.join("\n", output));
+		assertEquals(2, output.size());
+		assertTrue(output.contains("2016,g1,,v1_02,v1_03,v2_01,,v2_03"));
+		assertTrue(output.contains("2016,g2,v1_01,,v1_03,v2_01,v2_02,"));
+	}
+	
+	
 	@Test
-	public void testMultipleColumn() throws Exception {
-		String remoteCsvFolder = "/etltest/csvtranspose/data/";
-		String remoteCsvOutputFolder = "/etltest/csvtransposeoutput/";
-		String csvtransProp = "csvTransposeMultiple.properties";
-		String[] csvFiles = new String[] {"csvTransposeMultipleData.csv"};		
-		String schemaFolder = "/etltest/csvtranspose/cfg/"; //hardcoded in the properties
-		String schemaFile = "schemaSimple.txt";
-		getFs().copyFromLocalFile(false, true, new Path(this.getLocalFolder()+schemaFile), new Path(schemaFolder+schemaFile));
+	public void testPDEStat() throws Exception {
+		String remoteCsvFolder = "/etltest/csvtranspose/input/";
+		String remoteCsvOutputFolder = "/etltest/csvtranspose/output/";
+		String csvtransProp = "pdeStat.properties";
+		String[] csvFiles = new String[] {"STAT1027.Small_AJ2"};		
+		String cfgFolder = "/etltest/csvtranspose/cfg/"; //hardcoded in the properties
+
+		String[] cfgFiles=new String[]{"pde_state_table_fields_mapping.properties","pde_state_table_name_mapping.properties"};
+		for(String cfgFile:cfgFiles){
+			getFs().copyFromLocalFile(false, true, new Path(this.getLocalFolder()+cfgFile), new Path(cfgFolder+cfgFile));
+		}		
 		
 		List<String> output = super.mrTest(remoteCsvFolder, remoteCsvOutputFolder, csvtransProp, csvFiles, testCmdClass, false);
 		logger.info("Output is:\n{}",String.join("\n", output));
 		
-		assertEquals(3, output.size());
-		assertTrue(output.contains("name1,A,B,D,Very Good,Good,Not Bad"));
-		assertTrue(output.contains("name2,B,E,,Good,Bad,"));
-		assertTrue(output.contains("name3,,F,C,,Very Bad,Average"));
-	}
-	
-	public void test(){
-		//accept
-				TransposeSchema ts=new TransposeSchema();
-				Map<String, List<String>> a=new HashMap<String,List<String>>();
-				a.put("table1", Arrays.asList(new String[]{"abc","def","fgh"}));
-				a.put("table2", Arrays.asList(new String[]{"abc","def","fgh"}));
-				ts.setTableMap(a);
-				Map<String, List<String>> b=new HashMap<String,List<String>>();
-				b.put("abc", Arrays.asList(new String[]{"111","222","333"}));
-				b.put("abf", Arrays.asList(new String[]{"111","222","333"}));
-				ts.setTypeMap(b);
-				
-				ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-				try {
-					String json = ow.writeValueAsString(ts);
-					logger.info("json:\n{}", json);
-				} catch (JsonProcessingException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+		assertEquals(15,output.size());
+		assertTrue(output.contains("20161027,0000,016,CTXKEY,0000000073,,,,,0000000007,,,"));
+
 	}
 }
