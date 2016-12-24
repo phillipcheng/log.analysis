03f0594535b4c0be0236369f4af9888ce1e2a6d2
hanzac <hanzac@hotmail.com>
2016-12-24 13:59:06 +0800

    Update the unit test to junit cases

--- bdap-mgr/src/test/java/etl/flow/test/TestFTPDeployMethod.java
+++ bdap-mgr/src/test/java/etl/flow/test/TestFTPDeployMethod.java
@@ -3,30 +3,33 @@ package etl.flow.test;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
+import org.junit.Test;
+
 import etl.flow.deploy.FTPDeployMethod;
 
 public class TestFTPDeployMethod {
 
-	public static void main(String[] args) {
-		FTPDeployMethod m = new FTPDeployMethod("127.0.0.1", 21, "player", "123456");
+	@Test
+	public void test() {
+		FTPDeployMethod m = new FTPDeployMethod("192.85.247.104", 21, "dbadmin", "123456");
 		
-		m.copyFromLocalFile("pom.xml", "/user/player/pom.xml");
+		m.copyFromLocalFile("pom.xml", "/user/dbadmin/pom.xml");
 		
-		List<String> result = m.readFile("/user/player/pom.xml");
-		
-		System.out.println(result);
-		
-		m.delete("/user/player/pom.xml", true);
-		
-		m.createFile("/user/player/test.txt", "Hello World!".getBytes(StandardCharsets.UTF_8));
-		
-		result = m.readFile("/user/player/test.txt");
+		List<String> result = m.readFile("/user/dbadmin/pom.xml");
 		
 		System.out.println(result);
 		
-		m.delete("/user/player/test.txt", true);
+		m.delete("/user/dbadmin/pom.xml", true);
 		
-		m.listFiles("/user/player");
+		m.createFile("/user/dbadmin/test.txt", "Hello World!".getBytes(StandardCharsets.UTF_8));
+		
+		result = m.readFile("/user/dbadmin/test.txt");
+		
+		System.out.println(result);
+		
+		m.delete("/user/dbadmin/test.txt", true);
+		
+		m.listFiles("/user/dbadmin");
 	}
 
 }
