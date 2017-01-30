8c618ca544d1d314ced86bfaa6de408296709433
phillipcheng <phillipchengyi@gmail.com>
2017-01-30 06:35:51 -0800

    add scala implementation ACmd.scala, and DatasetSqlCmd.scala

--- bdaps.engine/src/main/scala/bdaps/engine/cmd/CsvTransformCmd.scala
+++
@@ -1,9 +0,0 @@
-package bdaps.engine.cmd
-
-class CsvTransformCmd(wfName:String, wfid:String, staticCfg:String, prefix:String, 
-      defaultFs:String) extends ACmd(wfName, wfid, staticCfg, prefix, defaultFs){
-  
-  override def init {
-    super.init
-  }
-}
