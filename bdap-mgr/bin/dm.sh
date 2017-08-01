e92ac79ea73a83578211350475f41fc48c79a2e9
zhen-zhao <zhen.zhao@hpe.com>
2017-08-01 16:13:15 +0800

    update dm.sh  version code

--- bdap-mgr/bin/dm.sh
+++ bdap-mgr/bin/dm.sh
@@ -1,4 +1,4 @@
 java_home=/usr/lib/jvm/java-1.8.0/
-bdap_home=/data/bdap-r0.3-jdk1.7
+bdap_home=/data/bdap-bdap-r0.6.0
 
 $java_home/bin/java -cp .:"$bdap_home/mgr/lib/*" etl.flow.deploy.FlowDeployer "$@"
